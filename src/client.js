cf8ed218221001290e710fe415733258110d822c
Guido Schmitz <hello@guidoschmitz.co>
2016-03-29 17:21:13 +0200

    flattened files

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from '../configure-store';
+import configureStore from './configure-store';
 
 export default ({ routes, reducers }) => {
   const store = configureStore(reducers);

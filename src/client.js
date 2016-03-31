de37437b28123607f4c5786cb7477c0c44ecd0d6
Guido Schmitz <hello@guidoschmitz.co>
2016-03-31 16:16:53 +0200

    lint errors

--- src/client.js
+++ src/client.js
@@ -3,11 +3,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { browserHistory } from 'react-router';
 import configureStore from './configure-store';
-import { syncHistoryWithStore } from 'react-router-redux'
+import { syncHistoryWithStore } from 'react-router-redux';
 
 export default ({ createRoutes, reducers }) => {
   const store = configureStore(reducers);
-  const history = syncHistoryWithStore(browserHistory, store)
+  const history = syncHistoryWithStore(browserHistory, store);
   const routes = createRoutes(history);
 
   render(
